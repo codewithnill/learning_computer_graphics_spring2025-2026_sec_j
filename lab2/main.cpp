d8e5746c13b8e41319e6585d50b7ea33969f5504
codewithnill <nbnill.hmlnmh@gmail.com>
2026-02-13 23:22:51 +0600

    axis added

---
+++ lab2/main.cpp
@@ -0,0 +1,45 @@
+#include <windows.h>
+#include <GL/gl.h> // for MS Windows
+#include <GL/glut.h> // GLUT, include glu.h and gl.h
+
+void display() {
+    glClearColor(0.5f, 0.5f, 0.5f, 0.0f); // Set background color to black and opaque
+    glClear(GL_COLOR_BUFFER_BIT); // Clear the color buffer (background)
+
+    gluOrtho2D(-250,250,-250,250);
+
+    glLineWidth(2);
+    glBegin(GL_LINES);
+    //glColor3f(1.0f, 1.0f, 0.0f);
+    glColor3f(0.059, 1, 0);
+    glVertex2f(-250,0);
+    glVertex2f(250,0);
+
+    //glColor3f(0.0f, 1.0f, 1.0f);
+    glColor3f(0.059, 1, 0);
+    glVertex2f(0,-250);
+    glVertex2f(0,250);
+
+    // extra line
+    //glColor3f(1.0f, 0.0f, 1.0f);
+    //glVertex2f(-200,-100);
+    //glVertex2f(200,100);
+
+    glEnd();
+
+    glFlush(); // Render now
+}
+
+
+/* Main function: GLUT runs as a console application starting at main() */
+int main(int argc, char** argv) {
+    //argc = argument count (number of command-line arguments)
+    //argv = argument vector (array of command-line argument strings)
+    glutInit(&argc, argv);
+    glutInitWindowSize(600, 600); // Set the window's initial width & height
+    glutInitWindowPosition(200, 100);  // Set the window's initial position according to the monitor
+    glutCreateWindow("Task 1"); // Create a window with the given title
+    glutDisplayFunc(display); // Register display callback handler for window re-paint
+    glutMainLoop(); // Enter the event-processing loop
+    return 0;
+}
