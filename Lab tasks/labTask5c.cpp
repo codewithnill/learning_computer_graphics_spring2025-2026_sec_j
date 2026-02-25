d27ab2bdfba0348458b78f0f5e7f49cb7a480797
codewithnill <nbnill.hmlnmh@gmail.com>
2026-02-25 15:24:32 +0600

    window size, ortho2d parameters and window position changed for south africa flag..

--- Lab tasks/labTask5c.cpp
+++ Lab tasks/labTask5c.cpp
@@ -5,10 +5,10 @@
 void southafrica(); // prototype
 
 void display() {
-    glClearColor(1, 1, 1, 1);
+    glClearColor(0 / 255.0f, 119 / 255.0f, 73 / 255.0f, 1);
     glClear(GL_COLOR_BUFFER_BIT); // Clear the color buffer (background) (fills the screen with this color)
 
-    gluOrtho2D(0, 660, 0, 480);
+    gluOrtho2D(0, 450, 0, 300);
     // Sets up a 2D orthographic projection
     // X-axis ranges from 0 to 660
     // Y-axis ranges from 0 to 480
@@ -21,6 +21,15 @@ void display() {
 
 void southafrica() {
 
+
+    // region A
+    // glBegin(GL_QUADS);
+    // glColor3f(1 / 255.0f, 1 / 255.0f, 1 / 255.0f);
+    // glVertex2f(450 * xpx, 120 * ypx);
+    // glVertex2f(234 * xpx, 120 * ypx);
+    // glVertex2f(54 * xpx, 0 * ypx);
+    // glVertex2f(450 * xpx, 0 * ypx);
+    // glEnd();
 }
 
 
@@ -31,8 +40,8 @@ int main(int argc, char** argv) {
     //argc = argument count (number of command-line arguments)
     //argv = argument vector (array of command-line argument strings)
     glutInit(&argc, argv);
-    glutInitWindowSize(660, 480); // Set the window's initial width & height
-    glutInitWindowPosition(380, 150);  // Set the window's initial position according to the monitor
+    glutInitWindowSize(450, 300); // Set the window's initial width & height
+    glutInitWindowPosition(430, 210);  // Set the window's initial position according to the monitor
     glutCreateWindow("Task 5c : South Africa flag"); // Create a window with the given title
     glutDisplayFunc(display); // Register display callback handler for window re-paint
     glutMainLoop(); // Enter the event-processing loop (keeps the program alive)
